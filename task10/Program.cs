3c67b7c9781d8ad9062094574b9ef5620603b5fd
Roman <vvptus@yandex.ru>
2022-10-13 00:00:42 +0300

    task10

---
+++ task10/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 3
+// Напишите программу, которая принимает на вход число (N) и
+// выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+
+int N = Prompt("Введите число   ");
+if (N < 1)
+    Console.WriteLine("Число меньше единицы, введите положительное число");
+int count = 1;
+int cubnamber = 0;
+while (count <= N)
+{
+    cubnamber = count * count * count;
+    System.Console.WriteLine($"{cubnamber}");
+    count = count + 1;
+}
---
+++ task9/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 2
+// Напишите программу, которая принимает на вход координаты двух точек
+// и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+// Math.Sqrt - квадратный корень
+
+int Prompt(string massage)
+{
+    Console.WriteLine(massage);
+    string number = Console.ReadLine();
+    int value = Convert.ToInt32(number);
+    return value;
+}
+
+const int x = 0;
+const int y = 1;
+const int z = 2;
+
+int[] point1 = InputPoint(1);
+int[] point2 = InputPoint(2);
+
+int[] InputPoint(int point)
+{
+    int[] result = new int[3];
+    result[x] = Prompt($"Введите х {point}   ");
+    result[y] = Prompt($"Введите y {point}   ");
+    result[z] = Prompt($"Введите z {point}   ");
+    return result;
+}
+
+int Power2(int arg)
+{
+    return arg * arg;
+}
+
+double length = Math.Sqrt(Power2(point1[x] - point2[x]) + Power2(point1[y] - point2[y]) + Power2(point1[z] - point2[z]));
+Console.WriteLine($"{length:f2}");
